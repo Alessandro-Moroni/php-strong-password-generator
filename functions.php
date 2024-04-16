08d5b79d7fee27e5d6f1bcbcf0b5ce44d70a24ad
Alessandro-Moroni <alemoro98@gmail.com>
2024-04-16 15:49:30 +0200

    [did]milestone-2

--- functions.php
+++ functions.php
@@ -1,4 +1,15 @@
 <?php
 
+function passwordRandom() {
+  $passString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?&%$<>^+-*/()[]{}@#_=";
+  $password = array();
+  $passLength = strlen($passString) - 1;
+  $randomNumber = $_GET['num_password'];
+  for ($i = 0; $i < $randomNumber; $i++) {
+      $num = rand(0, $passLength);
+      $password[] = $passString[$num];
+  }
+  return implode($password);
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 
-function passwordRandom() {
-  $passString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?&%$<>^+-*/()[]{}@#_=";
-  $password = array();
-  $passLength = strlen($passString) - 1;
-  $randomNumber = $_GET['num_password'];
-  for ($i = 0; $i < $randomNumber; $i++) {
-      $num = rand(0, $passLength);
-      $password[] = $passString[$num];
-  }
-  return implode($password);
-}
+require_once __DIR__ . './functions.php';
 
 
 ?>
